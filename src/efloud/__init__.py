e4a52340f94b80dfedf415a4167bb2b9cde1dcb7
Joseph Courtney <5942706+josephcourtney@users.noreply.github.com>
2026-09-10 20:29:00 -0400

    Expose declarative project API at package root

--- src/efloud/__init__.py
+++ src/efloud/__init__.py
@@ -3,13 +3,27 @@ from __future__ import annotations
 from importlib.metadata import version
 
 from efloud.api import Dataset, DatasetManifest, DatasetSpec, Engine, Repository, SyncResult
+from efloud.collections import CollectionContext, CollectionDefinition, CollectionInventory, CollectionItem
 from efloud.errors import DatasetError, EfloudError, ExecutionError, ExportError, RepositoryError, VerificationError
+from efloud.lockfile import LockfileError, ProjectLock
 from efloud.planning import SyncRequest
+from efloud.project import (
+    CollectionProvider,
+    Project,
+    ProjectError,
+    ProjectSchemaError,
+    ProviderResolutionError,
+)
 from efloud.sources import CollectionSource, HttpSource, LocalSource, RestSource, RsyncSource, Source
 
 __version__ = version("efloud")
 
 __all__ = [
+    "CollectionContext",
+    "CollectionDefinition",
+    "CollectionInventory",
+    "CollectionItem",
+    "CollectionProvider",
     "CollectionSource",
     "Dataset",
     "DatasetError",
@@ -21,6 +35,12 @@ __all__ = [
     "ExportError",
     "HttpSource",
     "LocalSource",
+    "LockfileError",
+    "Project",
+    "ProjectError",
+    "ProjectLock",
+    "ProjectSchemaError",
+    "ProviderResolutionError",
     "Repository",
     "RepositoryError",
     "RestSource",
